cb93fba1db96b2334ae681aaf512063ce623900e
nazkenya <nazkyakenya@gmail.com>
2023-11-22 10:05:28 +0700

    menambahkan CSRF_TRUSTED_ORIGINS di setting.py

--- tokobuahnazkya/settings.py
+++ tokobuahnazkya/settings.py
@@ -147,3 +147,4 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ['https://nazkya-raahiil-tugas.pbp.cs.ui.ac.id']
